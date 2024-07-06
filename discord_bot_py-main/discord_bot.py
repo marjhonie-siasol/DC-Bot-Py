39afb346da9e05e47601d0f4189d888bf454b610
Marjhonie <157345383+marjhonie-siasol@users.noreply.github.com>
2024-07-07 02:32:54 +0800

    Update discord_bot.py

--- discord_bot_py-main/discord_bot.py
+++ discord_bot_py-main/discord_bot.py
@@ -5,7 +5,6 @@ import requests
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
 @bot.event
 async def on_ready():
     print(f"{bot.user} has connected to the discord server")
